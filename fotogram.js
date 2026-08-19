ab1b82ab867173bec4dd9f319241ae1c27467c1f
wagnermichael97 <wagner.michael.kf@gmail.com>
2026-08-19 18:55:26 +0200

    imgs changed + css updated + js updated + html updated

--- fotogram.js
+++ fotogram.js
@@ -1,22 +1,29 @@
 let photos = [
-    "./assets/img/img_1.png",
-    "./assets/img/img_2.png",
-    "./assets/img/img_3.png",
-    "./assets/img/img_4.png",
-    "./assets/img/img_5.png",
-    "./assets/img/img_6.png",
-    "./assets/img/img_7.png",
-    "./assets/img/img_8.png",
-    "./assets/img/img_9.png",
-    "./assets/img/img_10.png",
-    "./assets/img/img_11.png",
-    "./assets/img/img_12.png"
+    "./assets/img/img_1_big.jpg",
+    "./assets/img/img_2_big.jpg",
+    "./assets/img/img_3_big.png",
+    "./assets/img/img_4_big.jpg",
+    "./assets/img/img_5_big.jpg",
+    "./assets/img/img_6_big.jpg",
+    "./assets/img/img_7_big.jpg",
+    "./assets/img/img_8_big.jpg",
+    "./assets/img/img_9_big.jpg",
+    "./assets/img/img_10_big.jpg",
+    "./assets/img/img_11_big.jpg",
+    "./assets/img/img_12_big.jpg",
+    
 ]
 
 let photoBoxEL = document.getElementById("photo-box");
+let widgetBoxEL = document.getElementById("widget-box");
 
 photoBoxEL.innerHTML = [];
 
-for (let i=0; i < photos.length; i++) {
-    photoBoxEL.innerHTML += `<img src="${photos[i]}">`;
+
+for (let i = 0; i < photos.length; i++) {
+    photoBoxEL.innerHTML += `<img src="${photos[i]}" onclick="openLarge(${i})">`;
+}
+
+function openLarge(index) {
+    widgetBoxEL.innerHTML = `<img src="${photos[index]}">`;
 }
